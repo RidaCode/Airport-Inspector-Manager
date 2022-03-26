cde427dbe37833c97b88a0fbf4a3028849e3d6d5
RidaCode <abdulredha@live.com>
2022-03-26 14:41:03 +0300

    fixed: no longer closing connection to db the cron job executed without db connection

--- backend/scraper/runCron.js
+++ backend/scraper/runCron.js
@@ -1,14 +1,4 @@
 const cron = require("node-cron");
-const mongoose = require("mongoose");
-
-// making sure we closed all connections before running cron
-disconnectDB();
-
-// close and disconnect
-async function disconnectDB() {
-  await mongoose.connection.close();
-  return await mongoose.disconnect();
-}
 
 // execute refreshDB.js every 1 minute
 cron.schedule("*/1 * * * *", function () {
@@ -19,13 +9,5 @@ cron.schedule("*/1 * * * *", function () {
   shell.series(commandList, function (err) {
     //    console.log('executed many commands in a row');
     console.log("Cron Job Done!");
-  });
-});
-
-// close connections to DB when the process ends
-process.on("SIGINT", () => {
-  mongoose.connection.close(() => {
-    console.log("Mongoose connection closed");
-    process.exit(0);
   });
 });
